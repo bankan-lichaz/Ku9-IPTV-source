0154e573a07012e101df94c9780aa3eb503e56d2
bankan-lichaz <130152591+bankan-lichaz@users.noreply.github.com>
2026-07-16 15:07:03 +0800

    Update update_zght.js

--- update_zght.js
+++ update_zght.js
@@ -34,7 +34,7 @@ function fetchWithTimeout(url, timeout = 5000) {
     //list = list.slice(-20);
 
     // ⭐ 取中开头50行
-    list = list.slice(0, 20);
+    list = list.slice(21, 50);
 
     fs.writeFileSync("ZGHT", list.join("\n"));
 
