b79c44b05a9a5b432b79748d94c1b74643dd3b81
bankan-lichaz <130152591+bankan-lichaz@users.noreply.github.com>
2026-07-18 20:59:52 +0800

    Update ITVlist1.py

--- ITVlist1.py
+++ ITVlist1.py
@@ -366,8 +366,6 @@ async def main():
             # parts[0] = "http:"
             # parts[1] = ""
             # parts[2] = "IP:端口"
-         def extract_server(url):
-            parts = url.split("/")
 
             # 写入 ZGHT2 文件
             with open("ZGHT2", "w", encoding="utf-8") as f:
