9d653f33be39f85451898835289ed7b9984293ef
bankan-lichaz <130152591+bankan-lichaz@users.noreply.github.com>
2026-07-30 16:56:45 +0800

    Update update_zght1.js

--- update_zght1.js
+++ update_zght1.js
@@ -22,7 +22,7 @@ async function fetchWithTimeout(url, timeout = 5000) {
     }
     
     // const blockList = ["219.137", "220.122"];
-    const blockList = ["219.137","221.232"];
+    const blockList = ["219.137","221.232","121.19","106.115"];
     let list = json.results
       .map(item => "http://" + item.host)
       .filter(Boolean)
